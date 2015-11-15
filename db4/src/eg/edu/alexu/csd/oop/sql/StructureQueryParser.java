08c8db72559a71d4e8cbf50b8c04e093dbbfa01a
Amira Mohammed <Amira patience.trust.in.God@gmail.com>
2015-11-14 23:45:16 -0800

    44

--- db4/src/eg/edu/alexu/csd/oop/sql/StructureQueryParser.java
+++ db4/src/eg/edu/alexu/csd/oop/sql/StructureQueryParser.java
@@ -129,7 +129,7 @@ public class StructureQueryParser extends MyParser{
 			// if success in creation the file 
 			if (f.exists()){
 				if (table_name.equals("table_name11")){
-					//log("table_name11 :" + attr,true);
+					log("table_name11 :" + attr,true);
 				}
 				new XmlWriter(f,new String[][]{},attr,table_name);
 				return true;
