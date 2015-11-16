d5a4d5743637eb0e378708d9ea2bfbea16da686e
Amira Mohammed <Amira patience.trust.in.God@gmail.com>
2015-11-16 12:52:10 -0800

    jjj

--- BBBB/src/eg/edu/alexu/csd/oop/sql/AAAEngine.java
+++ BBBB/src/eg/edu/alexu/csd/oop/sql/AAAEngine.java
@@ -45,8 +45,8 @@ public class AAAEngine implements Database{
 	}
 	@Override	
 	public boolean executeStructureQuery(String query) throws SQLException{
-		//log("",true);
-		log ("SQ: "+query+"\n",false);
+		log("",true);
+		//log ("SQ: "+query+"\n",false);
 		if (query==null)
 				throw new SQLException("Null Query "+query);
 		StructureQueryParser p = new StructureQueryParser(curdb);
