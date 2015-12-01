acb9b2928a2f45c4d492e6486918104d5c975432
Amira Mohammed <Amira patience.trust.in.God@gmail.com>
2015-11-30 22:57:18 -0800

    test

---
+++ Paint2/src/eg/edu/alexu/csd/oop/history/MyHistory.java
@@ -0,0 +1,11 @@
+package eg.edu.alexu.csd.oop.history;
+import java.util.ArrayList;
+import eg.edu.alexu.csd.oop.draw.Shape;
+public interface MyHistory {
+	public void add (ArrayList<Shape> a);
+	public ArrayList<Shape> cur();
+	public void undo ();
+	public void redo ();
+	public void clear();
+	public int size();
+}
